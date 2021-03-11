5daf185dff5f45ace2e24ac2637b7034a782b891
nobodynoticed <eugenejukov23@gmail.com>
2021-03-11 17:26:17 +0300

    feat: implement func reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    const isNum = isFinite(n);
+    if (isNum) {
+        let reversePosInt = n.toString().split("").reverse().join("");
+        if (reversePosInt.includes("-")) {
+            reversePosInt = reversePosInt.slice(0, reversePosInt.length - 1);
+        }
+        return +reversePosInt;
+    }
+};
